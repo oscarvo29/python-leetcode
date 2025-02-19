f9dfcd2dea928ba56dbac86ea27e54b3024580b5
oscarvo29 <oscar.vinther@gmail.com>
2025-02-19 15:58:05 +0100

    longest-common-prefix

--- longest-common-prefix.py
+++ longest-common-prefix.py
@@ -1,26 +1,37 @@
 
+"""
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+"""
 
 def longestCommonPrefix(strs: list[str]) -> str:
-    counter = 1
-    while True:
-        prefix = strs[0][:counter]
-
+    shortest_word = strs[0]
+    for word in strs:
+        if len(shortest_word) > len(word):
+            shortest_word = word
+    strs.remove(shortest_word)
+    prefix = ""
+    for idx, c in enumerate(shortest_word):
         for word in strs:
-            if len(word) <= 0:
-                return ""
-            if counter >= len(word) and counter != 1:
+            if word[idx] != c:
                 return prefix
+        prefix += c
 
-            if word[:counter] != prefix:
-                if counter == 1:
-                    return ""
-                return prefix[:-1]
-        counter += 1
+    return prefix
     
         
 l = ["flower","flow","flight"]
-print(longestCommonPrefix(l))
+output = longestCommonPrefix(l)
+ex = "fl"
+print(f"got: {output}, is correct = {output == ex}")
+
 l = ["a", "b"]
+output = longestCommonPrefix(l)
+ex = ""
+print(f"got: {output}, is correct = {output == ex}")
 
-
-print(longestCommonPrefix(l))
+l = ["dog", "racecar", "car"]
+output = longestCommonPrefix(l)
+ex = ""
+print(f"got: {output}, is correct = {output == ex}")
